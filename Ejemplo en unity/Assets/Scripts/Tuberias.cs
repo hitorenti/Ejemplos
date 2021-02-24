4467d1770eecccf1d47d5bacda91ab3b83bd8178
hitorenti <zmilingrs@gmail.com>
2021-02-24 15:41:18 -0600

    JJUGI

---
+++ Ejemplo en unity/Assets/Scripts/Tuberias.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tuberias : MonoBehaviour
+{
+    public float velocidad;
+
+    private void Update()
+    {
+        transform.position += Vector3.left * velocidad * Time.deltaTime;
+    }
+}
